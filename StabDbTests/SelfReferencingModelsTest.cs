2e21527e7643e2eedd196e4387d8ccdf808676bc
Yegor Sytnyk <yegor.sytnyk@gmail.com>
2014-01-10 11:24:22 +1100

    self-referencing models checked

---
+++ StabDbTests/SelfReferencingModelsTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StubDb;
+
+namespace StabDbTests
+{
+    [TestClass]
+    public class SelfReferencingModelsTest
+    {
+        #region Nested classes
+
+        public class Question
+        {
+            public int Id { get; set; }
+            public string Text { get; set; }
+            public Question DependantQuestion { get; set; }
+
+        }
+
+        public class TestContext : StubContext
+        {
+            public StubSet<Question> Questions { get; set; }
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void should_support_self_referencing_models()
+        {
+            var context = new TestContext();
+
+            var childChildQuestion = new Question() {Text = "Child-child question"};
+            var childQuestion = new Question() { Text = "Child question", DependantQuestion = childChildQuestion};
+            var parentQuestion = new Question() {Text = "Parent question", DependantQuestion = childQuestion};
+
+            context.Add(parentQuestion);
+
+            var childFromContext = context.Questions.Query().Single(x => x.Text == "Child question");
+            
+            Assert.IsNotNull(childFromContext.DependantQuestion);
+            Assert.AreEqual(childFromContext.DependantQuestion.Text, "Child-child question");
+        }
+    }
+}
--- StubDb/ModelStorage/EntityConnection.cs
+++ StubDb/ModelStorage/EntityConnection.cs
@@ -15,7 +15,7 @@ namespace StubDb.ModelStorage
 
         public EntityConnection(EntityTypeInfo type1, EntityTypeInfo type2, string name, int id1, int id2)
         {
-            if (IsDefaultTypeStoringOrder(type1, type2))
+            if (GetTypeSortingOrder(type1, type2) >= 0)
             {
                 TypeFirst = type1;
                 TypeSecond = type2;
@@ -36,14 +36,9 @@ namespace StubDb.ModelStorage
         {
         }
 
-        public static bool IsDefaultTypeStoringOrder(string typeNameFirst, string typeNameSecond)
+        protected static int GetTypeSortingOrder(EntityTypeInfo typeFirst, EntityTypeInfo typeSecond)
         {
-            return String.Compare(typeNameFirst, typeNameSecond, StringComparison.Ordinal) > 0;
-        }
-
-        public static bool IsDefaultTypeStoringOrder(EntityTypeInfo typeFirst, EntityTypeInfo typeSecond)
-        {
-            return IsDefaultTypeStoringOrder(typeFirst.GetId(), typeSecond.GetId());
+            return String.Compare(typeFirst.UniqueName, typeSecond.UniqueName, StringComparison.Ordinal);
         }
 
         public string GetUniqueKey()
